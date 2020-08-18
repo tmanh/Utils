43987c5513bce2d882fc96fed252b951a4701f34
Anh Truong <anhtruong@Anhs-MacBook-Pro.local>
2020-08-18 15:41:31 +0200

    updated hsv threshold tool

---
+++ Tools/hsv_threshold.py
@@ -0,0 +1,80 @@
+import cv2
+import sys
+import numpy as np
+
+
+def nothing(x):
+    pass
+
+
+# Create a window
+cv2.namedWindow('image')
+
+# create trackbars for color change
+cv2.createTrackbar('HMin', 'image', 0, 179, nothing)  # Hue is from 0-179 for Opencv
+cv2.createTrackbar('SMin', 'image', 0, 255, nothing)
+cv2.createTrackbar('VMin', 'image', 0, 255, nothing)
+cv2.createTrackbar('HMax', 'image', 0, 179, nothing)
+cv2.createTrackbar('SMax', 'image', 0, 255, nothing)
+cv2.createTrackbar('VMax', 'image', 0, 255, nothing)
+
+# Set default value for MAX HSV trackbars.
+cv2.setTrackbarPos('HMax', 'image', 179)
+cv2.setTrackbarPos('SMax', 'image', 255)
+cv2.setTrackbarPos('VMax', 'image', 255)
+
+# Initialize to check if HSV min/max value changes
+hMin = sMin = vMin = hMax = sMax = vMax = 0
+phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+
+# Output Image to display
+img = cv2.imread('/Users/anhtruong/Downloads/IMG_9728.jpg')
+output = img
+waitTime = 33
+
+
+while True:
+    # get current positions of all trackbars
+    hMin = cv2.getTrackbarPos('HMin', 'image')
+    sMin = cv2.getTrackbarPos('SMin', 'image')
+    vMin = cv2.getTrackbarPos('VMin', 'image')
+
+    hMax = cv2.getTrackbarPos('HMax', 'image')
+    sMax = cv2.getTrackbarPos('SMax', 'image')
+    vMax = cv2.getTrackbarPos('VMax', 'image')
+
+    # Set minimum and max HSV values to display
+    lower = np.array([hMin, sMin, vMin])
+    upper = np.array([hMax, sMax, vMax])
+
+    # Create HSV Image and threshold into a range.
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower, upper)
+    output = cv2.bitwise_and(img, img, mask=mask)
+
+    # Print if there is a change in HSV value
+    if (phMin != hMin) | (psMin != sMin) | (pvMin != vMin) | (phMax != hMax) | (psMax != sMax) | (pvMax != vMax):
+        phMin = hMin
+        psMin = sMin
+        pvMin = vMin
+        phMax = hMax
+        psMax = sMax
+        pvMax = vMax
+
+    scale_percent = 10.0
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    # resize image
+    resized = cv2.resize(output, dim, interpolation=cv2.INTER_AREA)
+
+    # Display output image
+    cv2.imshow('image', resized)
+
+    # Wait longer to prevent freeze for videos.
+    if cv2.waitKey(waitTime) & 0xFF == ord('q'):
+        break
+
+# Release resources
+cv2.destroyAllWindows()
